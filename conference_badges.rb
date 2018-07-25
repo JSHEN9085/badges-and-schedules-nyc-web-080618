601a41acb9984549d1e5de334fec594b29bd7d4c
JUN JIAN SHEN <jshen9085@gmail.com>
2018-07-25 02:08:02 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,12 @@ def batch_badge_creator(array)
   return new_array
 end
 
+def assign_rooms(array)
+  array2 = []
+  counter = 1
+  array.each do |name|
+    array2.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return array2
+end
